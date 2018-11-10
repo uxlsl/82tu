e500064f45218313aa268fe8d05c7a9e34c29581
lsl <songlin.lin@yundata.com>
2018-11-10 10:56:39 +0800

    增加中间件

--- a82tu/a82tu/pipelines.py
+++ a82tu/a82tu/pipelines.py
@@ -4,8 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import dataset
 
 
 class A82TuPipeline(object):
+    def __init__(self):
+        tmp = {
+                'title': '',
+                'info': '',
+                'url': ''}
+        db = dataset.connect('sqlite:///82tu.db')
+        self.table = db['movie']
+        self.table.upsert(tmp, ['url'])
+        self.table.create_index(['name', 'country'])
+
+
     def process_item(self, item, spider):
+        self.table.upsert(item, ['url'])
         return item
--- a82tu/a82tu/settings.py
+++ a82tu/a82tu/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'a82tu.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'a82tu.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'a82tu.pipelines.A82TuPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
